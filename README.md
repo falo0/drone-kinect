ab4bd821dc409ff0a85fcc31907df60cbc78c611
Zipixx <mail@zipixx.com>
2019-09-09 21:11:23 +0200

    initial marker generation, webcam marker detection and kinect camera image output

---
+++ README.md
@@ -0,0 +1,10 @@
+# Drone Kinect Tracking
+
+TODO project descr
+
+## Requirements
+
+python-numpy
+libfreenect2
+libfreenect2 python wrapper from https://github.com/37/py3freenect2/
+
---
+++ detect.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+import cv2
+
+cap = cv2.VideoCapture(0)
+
+while(True):
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    aruco_dict = cv2.aruco.Dictionary_get(cv2.aruco.DICT_6X6_250)
+    parameters =  cv2.aruco.DetectorParameters_create()
+    corners, ids, rejectedImgPoints = cv2.aruco.detectMarkers(gray, aruco_dict, parameters=parameters)
+    frame_markers = cv2.aruco.drawDetectedMarkers(frame.copy(), corners, ids)
+
+    cv2.imshow('frame_markers', frame_markers)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
+
